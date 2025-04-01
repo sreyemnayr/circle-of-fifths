553c6f240d3f727ffaadd9f9dc81fa977177b20f
Ryan Meyers <ryan@forkhunger.art>
2025-04-01 11:56:11 -0500

    Get rid of MyErrorHandler

--- src/app/api/burn/route.ts
+++ src/app/api/burn/route.ts
@@ -1,8 +1,7 @@
-import { MyErrorHandler } from '@/SpotifyProfile';
+// import { MyErrorHandler } from '@/SpotifyProfile';
 
 export async function GET(_request: Request) {
-  const eh = new MyErrorHandler()
-  eh.handleErrors("The app has exceeded its rate limits.")
-  return new Response("", { status: 200 })
+  // const eh = new MyErrorHandler()
+  // eh.handleErrors("The app has exceeded its rate limits.")
+  return new Response("", { status: 200 });
 }
-
